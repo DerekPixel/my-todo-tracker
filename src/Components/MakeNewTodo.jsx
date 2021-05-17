ba0e1f908338e6cc87884462430a733228f7d4f8
Derek Price <derekp098@gmail.com>
2021-05-16 23:11:56 -0700

    basic stuff

---
+++ src/Components/MakeNewTodo.jsx
@@ -0,0 +1,69 @@
+import {React, useState} from 'react'
+import PropTypes from 'prop-types'
+
+import {returnClonedArrayOfObjects} from '../functions.js'
+
+const MakeNewTodo = ({setArrayOfTodoObjects, arrayOfTodoObjects}) => {
+
+  const [todoTitle, setTodoTitle] = useState('');
+  const [todoInfo, setTodoInfo] = useState('')
+
+  function makeNewTodoObjectAndSaveToTheTodoArray() {
+
+    if(todoTitle !== '') {
+      var arrayOfTodoObjectsClone = returnClonedArrayOfObjects(arrayOfTodoObjects);
+
+      var newTodoObject = {
+        todoTitle,
+        todoInfo
+      }
+      
+      arrayOfTodoObjectsClone.push(newTodoObject);
+
+      setTodoTitle('');
+      setTodoInfo('');
+      setArrayOfTodoObjects(arrayOfTodoObjectsClone);
+    }
+
+    return;
+  }
+
+  return (
+    <div
+      className='make-new-todo'
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '10em'
+      }}
+    >
+
+      <label htmlFor="Todo-Title">Todo Title</label>
+      <input 
+        type="text" 
+        name="Todo-Title"
+        value={todoTitle}
+        onChange={(e) => setTodoTitle(e.target.value)}
+      />
+
+      <label htmlFor="Todo-Info">Todo Info</label>
+      <textarea 
+        name="Todo-Info"
+        value={todoInfo}
+        onChange={(e) => setTodoInfo(e.target.value)}
+      ></textarea>
+
+      <button
+        onClick={() => makeNewTodoObjectAndSaveToTheTodoArray()}
+      >Make New Todo</button>
+
+    </div>
+  )
+}
+
+MakeNewTodo.propTypes = {
+  setArrayOfTodoObjects: PropTypes.func,
+  arrayOfTodoObjects: PropTypes.array
+}
+
+export default MakeNewTodo
