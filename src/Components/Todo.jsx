83e2d1fd932e0b0f675a09ad2f3c4c7dcf3a754b
Derek Price <derekp098@gmail.com>
2021-05-17 23:56:36 -0700

    small change

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Todo = () => {
+const Todo = ({todoObject}) => {
   return (
-    <div>
-      
+    <div
+      className='todo'
+    >
+      <details>
+        <summary>
+          <div
+            className='todo-header'
+          >
+            <h2>{todoObject.todoTitle}</h2>
+            <button
+              className='todo-delete-btn'
+            >X</button>
+          </div>
+        </summary>
+
+        <div
+          className='todo-details'
+        >
+          <p>{todoObject.todoInfo}</p>
+        </div>
+      </details>
+
+
     </div>
   )
+}
+
+Todo.propTypes = {
+  todoObject: PropTypes.object,
 }
 
 export default Todo
